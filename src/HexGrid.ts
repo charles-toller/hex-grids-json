33fdf08bd6b11da343f4bc2d5a36708650bb52a9
Charles Toller <charles@toller.dev>
2020-03-23 23:04:04 -0600

    Add hex positional information

--- src/HexGrid.ts
+++ src/HexGrid.ts
@@ -1,5 +1,5 @@
 interface HexState<T> {
-    hexes: {[key: number]: {[key: number]: T}}
+    hexes: { [key: number]: { [key: number]: T } };
 }
 export interface SetAction {
     type: "HEX_GRIDS_SET";
@@ -20,8 +20,28 @@ export interface CreateHexAction {
     v: number;
     data: any;
 }
-export function hexGridsReducer(state = {hexes: {}}, action: SetAction | DeleteAction | CreateHexAction) {
-    switch (action.type) {
+const positional = {
+    northeast: [1, -1],
+    east: [1, 0],
+    southeast: [0, 1],
+    southwest: [-1, 1],
+    west: [-1, 0],
+    northwest: [0, -1],
+};
+const positionalKeys = Object.keys(positional);
+type Hex<T> = {
+    northeast: Hex<T>;
+    east: Hex<T>;
+    southeast: Hex<T>;
+    southwest: Hex<T>;
+    west: Hex<T>;
+    northwest: Hex<T>;
+} & T;
+export function hexGridsReducer(
+    state = { hexes: {} },
+    action?: SetAction | DeleteAction | CreateHexAction
+) {
+    switch (action?.type) {
         case "HEX_GRIDS_SET":
             return {
                 ...state,
@@ -30,14 +50,14 @@ export function hexGridsReducer(state = {hexes: {}}, action: SetAction | DeleteA
                     [action.u]: {
                         ...state?.hexes?.[action.u],
                         [action.v]: {
-                            ...state?.hexes?.[action.v]?.[action.v],
-                            [action.key]: action.value
-                        }
-                    }
-                }
-            }
+                            ...state?.hexes?.[action.u]?.[action.v],
+                            [action.key]: action.value,
+                        },
+                    },
+                },
+            };
         case "HEX_GRIDS_DELETE":
-            const hex = {...state?.hexes?.[action.u]?.[action.v]};
+            const hex = { ...state?.hexes?.[action.u]?.[action.v] };
             delete hex[action.key];
             return {
                 ...state,
@@ -45,10 +65,10 @@ export function hexGridsReducer(state = {hexes: {}}, action: SetAction | DeleteA
                     ...state?.hexes,
                     [action.u]: {
                         ...state?.hexes?.[action.u],
-                        [action.v]: hex
-                    }
-                }
-            }
+                        [action.v]: hex,
+                    },
+                },
+            };
         case "HEX_GRIDS_CREATE_HEX":
             return {
                 ...state,
@@ -56,72 +76,201 @@ export function hexGridsReducer(state = {hexes: {}}, action: SetAction | DeleteA
                     ...state?.hexes,
                     [action.u]: {
                         ...state?.hexes?.[action.u],
-                        [action.v]: action.data
-                    }
-                }
-            }
+                        [action.v]: action.data,
+                    },
+                },
+            };
     }
     return state;
 }
 export default class HexGrid<T extends {}> {
     private readonly stateRetriever: () => HexState<T>;
-    private readonly actionEmitter: (action: SetAction | DeleteAction | CreateHexAction) => any;
+    private readonly actionEmitter: (
+        action: SetAction | DeleteAction | CreateHexAction
+    ) => any;
     private readonly defaultHex: T;
-    private proxyCache: {[key: number]: {[key: number]: T | undefined} | undefined} = {};
+    private proxyCache: {
+        [key: number]: { [key: number]: Hex<T> | undefined } | undefined;
+    } = {};
     get state() {
         return this.stateRetriever();
     }
-    constructor(stateRetriever: () => {}, actionEmitter: (action: SetAction | DeleteAction | CreateHexAction) => any, defaultHex: T, path: string) {
-        const pathArr = path.split(".")
+    constructor(
+        stateRetriever: () => {},
+        actionEmitter: (
+            action: SetAction | DeleteAction | CreateHexAction
+        ) => any,
+        defaultHex: T,
+        path?: string
+    ) {
+        const pathArr = (path || "").split(".");
         this.stateRetriever = () => {
-            const state = stateRetriever();
+            const state = stateRetriever() as any;
+            if (typeof path === "undefined") return state;
             return pathArr.reduce((last, pathPart) => last[pathPart], state);
         };
         this.actionEmitter = actionEmitter;
         this.defaultHex = defaultHex;
     }
-    getHex(u: number, v: number): T {
+    getTessellationMatching(
+        u: number,
+        v: number,
+        cb: (tile: Hex<T>) => boolean,
+        ignoring: Array<[number, number]> = []
+    ): Array<[number, number]> {
+        const results: Array<[number, number]> = [];
+        ignoring.push([u, v]);
+        const tempState = this.stateRetriever().hexes;
+        if (cb(this.getHex(u, v))) {
+            results.push([u, v]);
+            for (let direction of positionalKeys) {
+                let hex = [
+                    u + positional[direction][0],
+                    v + positional[direction][1],
+                ];
+                if (ignoring.some((a) => a[0] === hex[0] && a[1] === hex[1]))
+                    continue;
+                if (tempState?.[hex[0]]?.[hex[1]] == null) continue;
+                results.push(
+                    ...this.getTessellationMatching(
+                        hex[0],
+                        hex[1],
+                        cb,
+                        ignoring
+                    )
+                );
+            }
+        }
+        return results;
+    }
+    iterateAllTiles<K>(
+        cb: (tile: Hex<T>) => K
+    ): { [key: number]: { [key: number]: K } } {
+        const state = this.stateRetriever().hexes;
+        const result = {};
+        for (let i in state) {
+            if (!state.hasOwnProperty(i)) continue;
+            result[i] = {};
+            for (let j in state[i]) {
+                if (!state[i].hasOwnProperty(j)) continue;
+                result[i][j] = cb(this.getHex(parseInt(i), parseInt(j)));
+            }
+        }
+        return result;
+    }
+    doesHexExist(u: number, v: number): boolean {
+        return this.stateRetriever()?.hexes?.[u]?.[v] != null;
+    }
+    getHex(u: number, v: number): Hex<T> {
         if (this.stateRetriever()?.hexes?.[u]?.[v] == null) {
             this.actionEmitter({
                 type: "HEX_GRIDS_CREATE_HEX",
                 u,
                 v,
-                data: {...this.defaultHex}
+                data: { ...this.defaultHex },
             });
         }
         if (this.proxyCache?.[u]?.[v] == null) {
             if (this.proxyCache[u] == null) {
                 this.proxyCache[u] = {};
             }
-            this.proxyCache[u]![v] = new Proxy<T>({} as any, {
+            this.proxyCache[u]![v] = new Proxy<Hex<T>>({} as any, {
                 get: (target: {}, p: PropertyKey) => {
+                    if (positionalKeys.some((a) => a === p)) {
+                        return this.getHex(
+                            u + positional[p][0],
+                            v + positional[p][1]
+                        );
+                    }
+                    if (p === "__hexGridsProxy") {
+                        return this.stateRetriever().hexes[u][v];
+                    }
                     return this.stateRetriever().hexes[u][v][p];
                 },
                 set: (target: {}, p: PropertyKey, value: any) => {
+                    if (
+                        typeof value === "object" &&
+                        value != null &&
+                        value.__hexGridsProxy
+                    ) {
+                        // Don't double proxy us
+                        value = value.__hexGridsProxy;
+                    }
                     this.actionEmitter({
                         type: "HEX_GRIDS_SET",
                         u,
                         v,
                         key: p,
-                        value
+                        value,
                     });
                     return true;
                 },
-                has: (target: T, p: PropertyKey) => {
+                has: (target: Hex<T>, p: PropertyKey) => {
                     return p in this.stateRetriever().hexes[u][v];
                 },
-                deleteProperty: (target: T, p: PropertyKey) => {
+                deleteProperty: (target: Hex<T>, p: PropertyKey) => {
                     this.actionEmitter({
                         type: "HEX_GRIDS_DELETE",
                         u,
                         v,
-                        key: p
+                        key: p,
                     });
                     return true;
                 },
                 ownKeys: () => {
-                    return Object.getOwnPropertyNames(this.stateRetriever().hexes[u][v]);
-                }
+                    return Object.getOwnPropertyNames(
+                        this.stateRetriever().hexes[u][v]
+                    );
+                },
+                getOwnPropertyDescriptor: (
+                    target: Hex<T>,
+                    p: PropertyKey
+                ): PropertyDescriptor | undefined => {
+                    return Object.getOwnPropertyDescriptor(
+                        this.stateRetriever().hexes[u][v],
+                        p
+                    );
+                },
+                getPrototypeOf: (target: Hex<T>): object | null => {
+                    return Object.getPrototypeOf(
+                        this.stateRetriever().hexes[u][v]
+                    );
+                },
+                setPrototypeOf: (target: Hex<T>, v: any): boolean => {
+                    return false;
+                },
+                isExtensible: (target: Hex<T>): boolean => {
+                    return Object.isExtensible(
+                        this.stateRetriever().hexes[u][v]
+                    );
+                },
+                preventExtensions: (target: Hex<T>): boolean => {
+                    Object.preventExtensions(this.stateRetriever().hexes[u][v]);
+                    return true;
+                },
+                defineProperty: (
+                    target: Hex<T>,
+                    p: PropertyKey,
+                    attributes: PropertyDescriptor
+                ): boolean => {
+                    Object.defineProperty(
+                        this.stateRetriever().hexes[u][v],
+                        p,
+                        attributes
+                    );
+                    return true;
+                },
+                apply: (target: Hex<T>, thisArg: any, argArray?: any): any => {
+                    return Function.prototype.apply.apply(
+                        this.stateRetriever().hexes[u][v] as any,
+                        [thisArg, argArray]
+                    );
+                },
+                construct: (target: Hex<T>, argArray: any): object => {
+                    return new (this.stateRetriever().hexes[u][v] as any)(
+                        ...argArray
+                    );
+                },
             });
         }
         return this.proxyCache[u]![v]!;
---
+++ tests/basic.spec.ts
@@ -0,0 +1,99 @@
+import HexGrid from "../src/HexGrid";
+import { hexGridsReducer } from "../src/HexGrid";
+
+test("Creates a hex grid", () => {
+    let state: any = hexGridsReducer();
+    const getState = () => state;
+    const actionHandler = jest.fn(
+        (action) => (state = hexGridsReducer(state, action))
+    );
+    const grid = new HexGrid<{ color: string }>(getState, actionHandler, {
+        color: "white",
+    });
+    expect(state).toStrictEqual({
+        hexes: {},
+    });
+    grid.getHex(0, 0);
+    expect(grid.getHex(0, 0)).toMatchObject({
+        color: "white",
+    });
+    expect(state).toMatchObject({
+        hexes: {
+            0: {
+                0: {
+                    color: "white",
+                },
+            },
+        },
+    });
+    expect(actionHandler.mock.calls).toMatchObject([
+        [
+            {
+                type: "HEX_GRIDS_CREATE_HEX",
+                u: 0,
+                v: 0,
+                data: {
+                    color: "white",
+                },
+            },
+        ],
+    ]);
+});
+test("creates tessellation", () => {
+    let state: any = hexGridsReducer();
+    const getState = () => state;
+    const actionHandler = jest.fn(
+        (action) => (state = hexGridsReducer(state, action))
+    );
+    const grid = new HexGrid<{ color: string }>(getState, actionHandler, {
+        color: "white",
+    });
+    const center = grid.getHex(0, 0);
+    center.color = "blue";
+    center.southeast.color = "blue";
+    center.east.color = "blue";
+    center.east.east.color = "red";
+    center.east.east.east.color = "blue";
+    expect(
+        grid.getTessellationMatching(0, 0, (tile) => tile.color === "blue")
+    ).toMatchObject([
+        [0, 0],
+        [1, 0],
+        [0, 1],
+    ]);
+});
+test("doesHexExist", () => {
+    let state: any = hexGridsReducer();
+    const getState = () => state;
+    const actionHandler = jest.fn(
+        (action) => (state = hexGridsReducer(state, action))
+    );
+    const grid = new HexGrid<{ color: string }>(getState, actionHandler, {
+        color: "white",
+    });
+    grid.getHex(0, 0);
+    expect(grid.doesHexExist(0, 0)).toBe(true);
+    expect(grid.doesHexExist(0, 1)).toBe(false);
+});
+test("iterateAllTiles", () => {
+    let state: any = hexGridsReducer();
+    const getState = () => state;
+    const actionHandler = jest.fn(
+        (action) => (state = hexGridsReducer(state, action))
+    );
+    const grid = new HexGrid<{ color: string }>(getState, actionHandler, {
+        color: "white",
+    });
+    grid.getHex(0, 0);
+    grid.getHex(0, 1).color = "blue";
+    grid.getHex(1, 0).color = "red";
+    expect(grid.iterateAllTiles((tile) => tile.color)).toMatchObject({
+        0: {
+            0: "white",
+            1: "blue",
+        },
+        1: {
+            0: "red",
+        },
+    });
+});
